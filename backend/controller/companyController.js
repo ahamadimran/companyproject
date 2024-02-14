6579d49f309a589c227ae0f38c9fa172b6695ad8
imranahmad369@gmail.com <ahamadimran369@gmail.com>
2024-02-14 16:51:56 +0530

    Create Search and File Upload Api

--- backend/controller/companyController.js
+++ backend/controller/companyController.js
@@ -1,8 +1,13 @@
+import { application } from "express";
 import company from "../model/companyModel.js";
 
 
 export const create = async(req, res) => {
     try {
+        req.body.fileName = req.file.filename;
+        req.body.filePath = req.file.path;
+        req.body.fileSize = req.file.size;
+        
         const companyData = new company(req.body);
         const {name} = companyData;
         const companyExist = await company.findOne({name});
@@ -47,3 +52,58 @@ export const update = async(req, res) => {
         res.status(500).json({error: "Internal Server Error"});
     }
 }
+
+
+export const deleteCompany = async(req, res) => {
+    try {
+        const id = req.params.id;
+        const companyExist = await company.findOne({_id: id})
+        if (!companyExist) {
+            return res.status(404).json({message: "Data not found"});
+        }
+        
+        await company.findByIdAndDelete(id);
+        res.status(201).json({message: "Company Deleted Successfull"});
+
+    } catch (error) {
+        res.status(500).json({error: "Internal Server Error"});
+    }
+}
+
+
+export const searchCompany = async(req, res) => {
+    try {
+        const {query} = req.body;
+
+        const filter = {
+            $or: [
+                {name: {$regex: query, $options: 'i'}},
+                {category: {$regex: query, $options: 'i'}},
+                {location: {$regex: `\\b${query}\\b`, $options: 'i'}},
+                {tags: {$regex: `\\b${query}\\b`, $options: 'i'}},
+                {description: {$regex: query, $options: 'i'}}
+            ]
+        }
+
+        const filterCompany = await company.find(filter);
+
+        if(filterCompany.length === 0) {
+            return res.status(404).json({message: "Data not found"})
+        }
+
+        return res.status(200).json(filterCompany)
+        
+    } catch (error) {
+        res.status(500).json({error: "Internal Server Error"});
+    }
+}
+
+// export const uploadFile = async(req, res) => {
+//     try {
+
+//         console.log(req.file);
+        
+//     } catch (error) {
+//         res.status(500).json({error: "Internal Server Error"});
+//     }
+// }
--- backend/routes/companyRoute.js
+++ backend/routes/companyRoute.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { create, fetch, update } from "../controller/companyController.js";
+import { create, deleteCompany, fetch, searchCompany, update } from "../controller/companyController.js";
+import uploadMiddleware from "../middleware/uploadMiddleware.js";
 
 const route = express.Router();
 
-route.post("/create", create);
+route.post("/create", uploadMiddleware.single("myfile"), create);
 route.get("/fetch", fetch);
 route.put("/update/:id", update);
+route.delete("/delete/:id", deleteCompany);
+route.post("/search", searchCompany);
+// route.post("/fileupload", uploadMiddleware.single("myfile"), uploadFile);
 
 
 export default route;
